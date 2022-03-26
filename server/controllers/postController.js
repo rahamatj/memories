f525ead9376f3781d316567ff025b5d1dc6591fa
Rahamat Jahan <rahamatj92@gmail.com>
2022-03-26 23:10:15 +0600

    Implement postController.getPosts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,5 +1,15 @@
-const getPosts = (req, res) => {
-    res.send("hello posts")
+import post from "../models/post.js"
+
+const getPosts = async (req, res) => {
+    try {
+        const posts = await post.find()
+
+        res.status(200).json(posts)
+    } catch (e) {
+        res.status(404).json({
+            message: e.message
+        })
+    }
 }
 
 export default {
